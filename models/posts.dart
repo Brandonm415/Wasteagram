24a168a70bf897b14690704f4d70a18b1205d25d
Brandon Mai <38685513+Brandonm415@users.noreply.github.com>
2021-09-21 19:31:18 -0700

    Added title to posts

---
+++ models/posts.dart
@@ -0,0 +1,10 @@
+//DTO for posts
+class Posts{
+  String dateTime;
+  int items;
+  String location;
+  String url;
+  String title;
+
+  Posts({this.dateTime,this.items,this.location, this.url, this.title});
+}
